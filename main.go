4eadd0e2d690604ebc3223905e3aee5c6fa26c7b
Jim Shaw <jimxshaw@gmail.com>
2023-08-02 15:18:31 -0500

    Refactor form to use flex box.

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 )
 
 const (
-	fieldWidth int  = 30
-	addKey     rune = 97
-	quitKey    rune = 113
+	fieldWidth     int    = 30
+	addKey         rune   = 97
+	quitKey        rune   = 113
+	menu           string = "Menu"
+	addNewContact  string = "Add New Contact"
+	asciiNumberOne int    = 49
 )
 
 var states = []string{"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
@@ -32,28 +35,43 @@ var contacts []Contact
 
 var app = tview.NewApplication()
 
+var pages = tview.NewPages()
+
+var form = tview.NewForm()
+
+var contactsList = tview.NewList().ShowSecondaryText(false)
+
+var flex = tview.NewFlex()
+
 var textView = tview.NewTextView().
 	SetTextColor(tcell.ColorWhite).
 	SetText("(a) to add a new contact \n(q) to quit")
 
-var form = tview.NewForm()
-
 func main() {
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	flex.SetDirection(tview.FlexRow).
+		AddItem(tview.NewFlex().AddItem(contactsList, 0, 1, true), 0, 6, false).
+		AddItem(textView, 0, 1, false)
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Rune() == quitKey {
 			app.Stop()
 		} else if event.Rune() == addKey {
+			form.Clear(true)
 			addNewContactForm()
+			pages.SwitchToPage(addNewContact)
 		}
 		return event
 	})
 
-	if err := app.SetRoot(textView, true).EnableMouse(true).Run(); err != nil {
+	pages.AddPage(menu, flex, true, true)
+	pages.AddPage(addNewContact, form, true, false)
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
 
-func addNewContactForm() {
+func addNewContactForm() *tview.Form {
 	contact := Contact{}
 
 	form.AddInputField("First Name", "", fieldWidth, nil, func(firstName string) {
@@ -86,5 +104,17 @@ func addNewContactForm() {
 
 	form.AddButton("Save", func() {
 		contacts = append(contacts, contact)
+		addContactList()
+		pages.SwitchToPage(menu)
 	})
+
+	return form
+}
+
+func addContactList() {
+	contactsList.Clear()
+
+	for index, contact := range contacts {
+		contactsList.AddItem(contact.firstName+" "+contact.lastName, " ", rune(asciiNumberOne+index), nil)
+	}
 }
