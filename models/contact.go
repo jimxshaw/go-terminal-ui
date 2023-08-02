5c00b766efe7aa0443f9f21c26de4abead0ae83a
Jim Shaw <jimxshaw@gmail.com>
2023-08-02 15:48:26 -0500

    Refactor logic into different files.

---
+++ models/contact.go
@@ -0,0 +1,12 @@
+package models
+
+// Contact represents a contact.
+type Contact struct {
+	FirstName   string
+	LastName    string
+	Email       string
+	PhoneNumber string
+	City        string
+	State       string
+	Business    bool
+}
