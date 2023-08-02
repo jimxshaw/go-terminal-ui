da5e5fb36f3fc5d3dedf287f2a2a1bd27e1f0638
Jim Shaw <jimxshaw@gmail.com>
2023-08-02 11:27:56 -0500

    Update main.go to instantiate new app.

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
 package main
 
+import "github.com/rivo/tview"
+
+// Contact represents a contact.
+type Contact struct {
+	firstName   string
+	lastName    string
+	email       string
+	phoneNumber string
+	city        string
+	state       string
+	occupation  string
+}
+
+var contacts []Contact
+
+var app = tview.NewApplication()
+
+func main() {
+	if err := app.SetRoot(tview.NewBox(), true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
